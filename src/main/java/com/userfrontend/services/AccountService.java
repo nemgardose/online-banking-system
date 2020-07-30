50503aa600518158f20b962375bb9ab93fa21215
nemgardose <gardosenemwel@gmail.com>
2020-07-30 23:49:07 +0800

    Added Withdraw Transaction Functionality

--- src/main/java/com/userfrontend/services/AccountService.java
+++ src/main/java/com/userfrontend/services/AccountService.java
@@ -3,11 +3,14 @@ package com.userfrontend.services;
 import java.security.Principal;
 
 import com.userfrontend.model.PrimaryAccount;
+import com.userfrontend.model.PrimaryTransaction;
 import com.userfrontend.model.SavingsAccount;
+import com.userfrontend.model.SavingsTransaction;
 
 public interface AccountService {
 	PrimaryAccount createPrimaryAccount();
 	SavingsAccount createSavingsAccount();
 	void deposit(String accountType, double amount, Principal principal);
 	void withdraw(String accountType, double amount, Principal principal);
+
 }
